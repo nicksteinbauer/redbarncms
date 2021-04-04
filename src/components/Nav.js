d9d79bf37e5a27e87079a93fd229abe7cab0912b
Nick <web@obrien.com>
2021-04-04 12:46:19 -0400

    commiting changes, Header and Hero. As well as Yaml Changes

---
+++ src/components/Nav.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'gatsby'
+
+function Nav() {
+    return (
+
+        <div className="navbar-start has-text-centered flex-md">
+            <Link className="navbar-item" to="/about">
+                <span>
+                About
+                </span>
+            </Link>
+            <Link className="navbar-item" to="/products">
+                <span>
+                Products
+                </span>
+            </Link>
+            <Link className="navbar-item" to="/blog">
+                <span>
+                Blog
+                </span>
+            </Link>
+            <Link className="navbar-item" to="/contact">
+                <span>
+                Contact
+                </span>
+            </Link>
+            <Link className="navbar-item" to="/contact/examples">
+                <span>
+                Form Examples
+                </span>
+            </Link>
+
+        </div>
+
+    )
+};
+
+export default Nav
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,42 +2,28 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../img/RedBarn-Logo.svg'
+import Nav from './Nav'
 
 import facebook from "../img/social/facebook.svg";
 import instagram from "../img/social/instagram.svg";
 import twitter from "../img/social/twitter.svg";
 
 const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: false,
-      navBarActiveClass: '',
-    }
+  state = {
+    open: false,
   }
+  handleButtonClick = () => {
+      this.setState(state => {
+        return {
+          open: !state.open,
+        };
+      });
+    };
 
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: 'is-active',
-            })
-          : this.setState({
-              navBarActiveClass: '',
-            })
-      }
-    )
-  }
 
   render() {
     return (
+      <>
       <nav
         className="navbar is-transparent"
         role="navigation"
@@ -48,59 +34,57 @@ const Navbar = class extends React.Component {
             <Link to="/" className="navbar-item" title="Logo">
               <img src={logo} alt="Red Barn Engineering" />
             </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
           </div>
+
+
+          <div className={this.state.open ? "hamburger active" : "hamburger" }>
+
+            <div className="ham-trigger flex-vertical" onClick={this.handleButtonClick} onKeyDown={this.handleButtonClick} role="button" tabIndex="0">
+                <div className={this.state.open ? "ham-button active" : "ham-button" }>
+                    <span className="line line-1"></span>
+                    <span className="line line-2"></span>
+                    <span className="line line-3"></span>
+                </div>
+            </div>
+
+            </div>
+
+        
           <div
             id="navMenu"
-            className={`navbar-menu justify-center flex-sm ${this.state.navBarActiveClass}`}
+            className="navbar-menu justify-center flex-sm"
           >
-            <div className="navbar-start has-text-centered always-flex">
-                <Link className="navbar-item flex-vertical" to="/about">
-                  <span>
-                    About
-                  </span>
-                </Link>
-                <Link className="navbar-item flex-vertical" to="/products">
-                  <span>
-                    Products
-                  </span>
-                </Link>
-                <Link className="navbar-item flex-vertical" to="/blog">
-                  <span>
-                    Blog
-                  </span>
-                </Link>
-                <Link className="navbar-item flex-vertical" to="/contact">
-                  <span>
-                    Contact
-                  </span>
-                </Link>
-                <Link className="navbar-item flex-vertical" to="/contact/examples">
-                  <span>
-                    Form Examples
-                  </span>
-                </Link>
 
-            </div>
+            <Nav />
             
+            <div className="social always-flex mobile">
+                <a target="_blank" className="flex-vertical" rel="noreferrer" href="https://www.facebook.com/barnesnursery"><img src={facebook} alt="Facebook Link" /></a>
+                <a target="_blank" className="flex-vertical" rel="noreferrer" href="https://www.instagram.com/barnes_nursery/"><img src={instagram} alt="Instagram Link" /></a>
+                <a target="_blank" className="flex-vertical" rel="noreferrer" href="https://twitter.com/barnesnursery"><img src={twitter} alt="Twitter Link" /></a>
+            </div>
+
           </div>
 
-          <div className="social always-flex">
+          <div className="social always-flex desktop">
                 <a target="_blank" className="flex-vertical" rel="noreferrer" href="https://www.facebook.com/barnesnursery"><img src={facebook} alt="Facebook Link" /></a>
                 <a target="_blank" className="flex-vertical" rel="noreferrer" href="https://www.instagram.com/barnes_nursery/"><img src={instagram} alt="Instagram Link" /></a>
                 <a target="_blank" className="flex-vertical" rel="noreferrer" href="https://twitter.com/barnesnursery"><img src={twitter} alt="Twitter Link" /></a>
             </div>
         </div>
       </nav>
+        
+      {this.state.open && (
+        <menu className="navigation">
+            <div className="nav-buffer">
+                <div className="inside-md flex-vertical-modified">
+                    
+                    <Nav />
+                    
+                </div>
+            </div>
+        </menu>
+      )}
+      </>
     )
   }
 }
