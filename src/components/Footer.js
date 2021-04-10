7d2bd3fa6e064d8e754873d43243e58aa417cfe6
Nick <web@obrien.com>
2021-04-10 19:53:41 -0400

    footer styles and other

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -46,23 +46,20 @@ const Footer = class extends React.Component {
                 </nav>
               </div>
               <div className="twenty">
-                <nav className="footer-menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </nav>
+                <h4>Headquarters</h4>
+                <p>
+                  6610 NE 181st St., Ste. 2<br/>
+                  Kenmore, WA 98028<br/>
+                  425.419.4979 (office)
+                </p>
               </div>
               <div className="twenty">
-                
+              <h4>Sandusky Office</h4>
+                <p>
+                  326 East Market St<br/>
+                  Sandusky, OH 44870<br/>
+                  425.535.7230 (mobile)
+                </p>
               </div>
         </div>   
       </footer>
