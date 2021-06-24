0f60d7595adc738c4cecd35bd5a79e416ae5d326
Nick <web@obrien.com>
2021-06-24 11:54:57 -0400

    deleted product page and all associate shite

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,7 +4,7 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+//import ProductPagePreview from './preview-templates/ProductPagePreview'
 //import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
